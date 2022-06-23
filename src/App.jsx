ba5d7173ce9b2c01665968eb2d4549544578c43a
emrergin <emrergin2757@yahoo.com>
2022-06-23 13:49:56 +0300

    image links

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { Icon } from '@iconify/react';
 import { Link } from "react-router-dom";
 import './App.css'
-import myFace from '../assets/proPic.png';
+import myFace from './assets//proPic.png';
 
 
 function App() {
