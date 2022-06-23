5bd35c6d70f40578b48fc8ce261b18e3b200c7dc
emrergin <emrergin2757@yahoo.com>
2022-06-23 11:59:18 +0300

    better build settings

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:emrergin/personalpage_react.git main:gh-pages
+# git push -f git@github.com:emrergin/personalpage_react.git main:gh-pages
 
 cd -
